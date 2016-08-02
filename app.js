e12427c784418d41eac4d9d86479af618588558b
JBeckius <jbeckius91@gmail.com>
2016-08-02 09:16:35 -0400

    basic functionality completed.

--- app.js
+++ app.js
@@ -1,11 +1,23 @@
 //server.js
-var http = require('http');
+// var http = require('http');
 var getInfo = require('./info.js');
+var express = require('express');
+var app = express();
 
-http.createServer(
+app.get('/', function (req, res) {
+  res.send(getInfo(0));
+});
 
-function onRequest(request, response) {
-  response.writeHead(200, {'Content-type': 'text/plain'});
-  response.write(getInfo(1));
-  response.end();
-}).listen(8080);
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Example app listening at http://%s:%s', host, port)
+});
+// http.createServer(
+//
+// function onRequest(request, response) {
+//   response.writeHead(200, {'Content-type': 'text/plain'});
+//   response.write(getInfo(1));
+//   response.end();
+// }).listen(8080);
